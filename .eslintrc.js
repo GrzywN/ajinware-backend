063b0ce0809667000a1b69febcb9cb978f4729d8
GrzywN <karolbinkowski3@proton.me>
2024-09-21 19:37:51 +0200

    feat: add typescript, configure eslint, prettier and jest
    - Configure eslint with prettier and typescript
    - Configure prettier with plugin for organizing imports
    - Configure jest to work with typescript
    - Add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
   root: true,
   env: {
+    es2020: true,
+    node: true,
     jest: true,
   },
-  extends: 'airbnb-base',
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
   rules: {
-    'no-underscore-dangle': 0,
-    'no-param-reassign': 0,
-    'no-return-assign': 0,
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0,
+    "no-return-assign": 0,
     camelcase: 0,
   },
 };
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,11 +1,13 @@
-function notFound(req, res, next) {
+import { NextFunction, Request, Response } from 'express';
+
+function notFound(req: Request, res: Response, next: NextFunction): void {
   res.status(404);
   const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
   next(error);
 }
 
 /* eslint-disable no-unused-vars */
-function errorHandler(err, req, res, next) {
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   /* eslint-enable no-unused-vars */
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
@@ -15,7 +17,4 @@ function errorHandler(err, req, res, next) {
   });
 }
 
-module.exports = {
-  notFound,
-  errorHandler,
-};
+export { errorHandler, notFound };
